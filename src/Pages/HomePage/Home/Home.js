1409489da73646ef54687b5acad89b6dacfca8a3
parvez13913 <parvezz13913@gmail.com>
2022-08-12 03:34:16 -0700

    temporaryIncome component complete

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Partners from '../Partners/Partners';
+import './Home.css';
+import TempoIncome from '../TempoIncome/TempoIncome';
 
 const Home = () => {
     return (
-        <div>
+        <div className='body-background'>
+            <TempoIncome></TempoIncome>
             <Partners></Partners>
         </div>
     );
--- src/Pages/HomePage/TempoIncome/TempoIncome.js
+++ src/Pages/HomePage/TempoIncome/TempoIncome.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import home1 from '../../../assets/temporaryIncome/home1.png';
+import home2 from '../../../assets/temporaryIncome/home2.png';
+import home3 from '../../../assets/temporaryIncome/home3.png';
+import home4 from '../../../assets/temporaryIncome/home4.png';
 
 const TempoIncome = () => {
     return (
-        <div>
-            <h2>Temporary income</h2>
+        <div className='container'>
+            <h2 className='uppercase text-base font-semibold mb-4 mt-8'>Temporary income</h2>
+            <div className='grid grid-cols-2 lg:grid-cols-4 md:grid-cols-4 gap-7'>
+                <div className='p-4 rounded-lg shadow-lg bg-white mb-3'>
+                    <h5 className="mb-0 font-weight-normal">$17.17</h5>
+                    <p className="text-mute small text-secondary">Earnings Today</p>
+                </div>
+                <div className='p-4 rounded-lg shadow-lg bg-white mb-3'>
+                    <h5 className="mb-0 font-weight-normal">$17.17</h5>
+                    <p className="text-mute small text-secondary">Today's commission</p>
+                </div>
+                <div className='p-4 rounded-lg shadow-lg bg-white mb-3'>
+                    <h5 className="mb-0 font-weight-normal">$17.17</h5>
+                    <p className="text-mute small text-secondary">Temporary income</p>
+                </div>
+                <div className='p-4 rounded-lg shadow-lg bg-white mb-3'>
+                    <h5 className="mb-0 font-weight-normal">$0</h5>
+                    <p className="text-mute small text-secondary">Interest</p>
+                </div>
+            </div>
+            <div className='grid grid-cols-4 lg:grid-cols-4 md:grid-cols-4 gap-7'>
+                <div>
+                    <img src={home1} alt="homeImage" />
+                </div>
+                <div>
+                    <img src={home2} alt="homeImage" />
+                </div>
+                <div>
+                    <img src={home3} alt="homeImage" />
+                </div>
+                <div>
+                    <img src={home4} alt="homeImage" />
+                </div>
+            </div>
         </div>
     );
 };
