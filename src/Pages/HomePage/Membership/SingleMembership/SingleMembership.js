c62129348457783ac4279716db71e2baeecd851c
abir14016 <abir14016@gmail.com>
2022-08-14 19:02:30 +0600

    membership done

--- src/Pages/HomePage/Membership/SingleMembership/SingleMembership.js
+++ src/Pages/HomePage/Membership/SingleMembership/SingleMembership.js
@@ -1,13 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-const SingleMembership = (singleMembership) => {
-    const { image, level, commission } = singleMembership;
-    return (
-        <div>
-            <h2>{commission}</h2>
-            <img src={image} alt="" />
-        </div>
-    );
-};
+// const SingleMembership = ({ singleMembership }) => {
+//     const { isCurrent, image, level, reqInvestment, commission, order, bgStart, bgEnd } = singleMembership;
+//     const join = "Join>>";
+//     const CurrentLevel = "Current Level";
+//     return (
+//         <div className={`flex justify-between mb-4 bg-gradient-to-r from-[${bgStart}] to-[${bgEnd}]`}>
+//             <div className='flex justify-center'>
+//                 <div>
+//                     <img src={image} alt="level" />
+//                 </div>
+//                 <div>
+//                     <h1>{level}</h1>
+//                     <p>Required Investment ${reqInvestment}</p>
+//                     <p>Commission {commission}% | {order} Orders</p>
+//                     <p>{bgStart},{bgEnd}</p>
+//                 </div>
+//             </div>
 
-export default SingleMembership;
+//             <div>
+//                 {
+//                     isCurrent ? <h2>{CurrentLevel}</h2> : <h2>{join}</h2>
+//                 }
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default SingleMembership;
