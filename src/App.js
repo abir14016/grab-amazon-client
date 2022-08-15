61e71793904a57b4f59412863a3cf021b36ecaff
parvez13913 <parvezz13913@gmail.com>
2022-08-15 01:49:27 -0700

    NotFound page design 50% complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Platfom from './Pages/HomePage/TempoIncome/Platfom/Platfom';
 import VipMember from './Pages/HomePage/TempoIncome/VipMember/VipMember';
 import Cooperation from './Pages/HomePage/TempoIncome/Cooperation/Cooperation';
 import Certificate from './Pages/HomePage/TempoIncome/Certificate/Certificate';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/vipMember' element={<VipMember></VipMember>}></Route>
         <Route path='/cooperation' element={<Cooperation></Cooperation>}></Route>
         <Route path='/certificate' element={<Certificate></Certificate>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import notfoundBackground from '../../../assets/notfound/notFoundBackground.png';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+    const myStyle = {
+        backgroundImage: `url(${notfoundBackground})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: 'cover'
+
+    }
     return (
-        <div>
-            404 error.
-            <h2>Tui shala vul pothe ascis.</h2>
+        <div style={myStyle} className='h-screen flex items-end justify-center pb-10'>
+            <div>
+                <h5 className='text-center mb-4 text-xl'>Sorry, we can't find that page! Don't worry though, everything is STILL AWESOME!</h5>
+                <Link to='/' className='btn btn-primary text-white'>Go Back Home</Link>
+            </div>
         </div>
     );
 };
