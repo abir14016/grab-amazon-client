cb61c2aab3865fd9a2aff73ddf76c0a92f4e402d
parvez13913 <parvezz13913@gmail.com>
2022-08-15 02:43:19 -0700

    Notfound page complete

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import notfoundBackground from '../../../assets/notfound/notFoundBackground.png';
 import { Link } from 'react-router-dom';
+import notFound from '../../../assets/notfound/notFound.png';
 
 const NotFound = () => {
-    const myStyle = {
-        backgroundImage: `url(${notfoundBackground})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: 'cover'
-
-    }
     return (
-        <div style={myStyle} className='h-screen flex items-end justify-center pb-10'>
+        <div className='h-screen flex items-center justify-center pb-10 bg-[#b8e1ec]'>
             <div>
+                <div>
+                    <img className='w-[200px] mx-4' src={notFound} alt="notFoundImage" />
+                </div>
+            </div>
+            <div>
+                <h1 className='text-9xl mb-10 font-serif'>
+                    <span className='text-[#f0e395]'>4</span>
+                    <span className='text-[#feb485]'>0</span>
+                    <span className='text-[#d15c95]'>4</span>
+                </h1>
                 <h5 className='text-center mb-4 text-xl'>Sorry, we can't find that page! Don't worry though, everything is STILL AWESOME!</h5>
                 <Link to='/' className='btn btn-primary text-white'>Go Back Home</Link>
             </div>
